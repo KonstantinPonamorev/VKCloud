8396518548791ba973b519d5c37beb70c5ba156c
Konstantin Ponamorev <kostyaponamorev@gmail.com>
2022-01-04 15:38:11 +0300

    Starting Commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import requests
+import time
+from pprint import pprint
+
+with open('token.txt', 'r') as file:
+    token = file.read().strip()
+
+class VkUser:
+    url = 'https://api.vk.com/method/'
+    def __init__(self, token, version='5.131'):
+        self.params = {
+            'access_token': token,
+            'v': version
+        }
+    def get_photos(self):
+        get_photos_url = self.url + 'photos.get'
+        get_photos_params = {
+            'owner_id': 'begemot_korovin',
+            'album_id': 'profile',
+            'extended': '1'
+        }
+        res = requests.get(get_photos_url, params={**self.params, **get_photos_params}).json()
+        pprint(res)
+
+Kostya = VkUser(token)
+Kostya.get_photos()
+
+
+
+
+
